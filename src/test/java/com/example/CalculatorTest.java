522d0896ae542a9632888ff2e3329eb72deb2f91
Sowbagya V S <sowbagyasivakumar@gmail.com>
2026-02-14 07:53:21 +0530

    initial commit

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2,3));
+    }
+}
