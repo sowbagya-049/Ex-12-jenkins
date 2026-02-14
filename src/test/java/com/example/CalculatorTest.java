2cf56b32ea619313e1a2fcbbb3f464d8b8d9e3da
sowbagya <sowbagyasivakumar@gmail.com>
2026-02-14 15:37:24 +0530

    Fix expected result in add method test

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -8,6 +8,6 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(10, calc.add(2,3));
+        assertEquals(5, calc.add(2,3));
     }
 }
